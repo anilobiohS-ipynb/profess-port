132ed2e7b17a627588d7fb54b13847713b792ae4
anilobiohS-ipynb <67705355+anilobiohS-ipynb@users.noreply.github.com>
2021-08-24 01:09:02 +0530

    update

--- index.js
+++ index.js
@@ -14,3 +14,26 @@ window.onscroll = function(){
 
 	}
 }
+
+var mySwiper = new Swiper(".swiper-container", {
+            // Optional parameters
+            spaceBetween: 5,
+            slidesPerView: 2,
+            loop: false,
+            freeMode: true,
+            loopAdditionalSlides: 5,
+            speed: 500,
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            breakpoints: {
+                // when window width is >= 640px
+                640: {
+                    slidesPerView: 5,
+                    slidesPerGroup: 5,
+                    freeMode: false
+                }
+            }
+        })
